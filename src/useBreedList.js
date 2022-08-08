e553bbf2d8047e1ef2b9bbc685147b52b82abe0a
DuskaT021 <aleksa2601@gmail.com>
2022-08-09 01:20:00 +0200

    added a comment for custom hooks

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 
 const localCache = {};
-
+// revisting the custom hooks chapter 
+// https://frontendmasters.com/courses/complete-react-v7/custom-hooks/
+// 2:17
+// https://github.com/btholt/citr-v7-project/blob/main/06-custom-hooks/src/useBreedList.js
 export default function useBreedList(animal) {
     const [breedList, setBreedList] = useState([]);
     const [status, setStatus] = useState("unloaded");
